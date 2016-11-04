a4c58ce7cb9f8b9290cbb2db26111bfb405fd5c0
JamesQAQ <luniacslime@gmail.com>
2016-11-04 06:13:16 +0000

    Update: add permission checking for manage of qa and poll
    As title.
    
    TEST=Runable

--- python/View.py
+++ python/View.py
@@ -42,16 +42,26 @@ class PollHandler(RequestHandler):
     async def post(self):
         self.set_header('Content-Type', 'application/json')
         db = await self.get_db()
-        data = []
-        async for row in db.execute(
-            'SELECT * FROM "poll" WHERE "status"=1'
-            ' ORDER BY "year" DESC, "order"'
-        ):
-            element = {}
-            for key in row:
-                element[key] = row[key]
-            data.append(element)
-        self.write({'data': data})
+        uid = self.get_secure_cookie('uid')
+
+        if uid == None:
+            self.write({'status': 'NOT LOGINED'})
+        else:
+            uid = int(uid)
+            user = await get_user(db, uid)
+            if user.power != 1:
+                self.write({'status': 'PERMISSION DENIED'})
+            else:
+                data = []
+                async for row in db.execute(
+                    'SELECT * FROM "poll" WHERE "status"=1'
+                    ' ORDER BY "year" DESC, "order"'
+                ):
+                    element = {}
+                    for key in row:
+                        element[key] = row[key]
+                    data.append(element)
+                self.write({'status': 'SUCCESS', 'data': data})
         await db.close()
 
 
@@ -59,17 +69,27 @@ class PollDeleteHandler(RequestHandler):
     async def post(self):
         self.set_header('Content-Type', 'application/json')
         db = await self.get_db()
-        try:
-            poll_id = self.get_argument('id')
-            await db.execute(
-                'UPDATE "poll" SET "status"=0 WHERE "id"=%s',
-                (poll_id, )
-            )
-            self.write({'status': 'SUCCESS'})
-        except Exception as e:
-            if DEBUG:
-                print(e)
-            self.write({'status': 'ERROR'})
+        uid = self.get_secure_cookie('uid')
+
+        if uid == None:
+            self.write({'status': 'NOT LOGINED'})
+        else:
+            uid = int(uid)
+            user = await get_user(db, uid)
+            if user.power != 1:
+                self.write({'status': 'PERMISSION DENIED'})
+            else:
+                try:
+                    poll_id = self.get_argument('id')
+                    await db.execute(
+                        'UPDATE "poll" SET "status"=0 WHERE "id"=%s',
+                        (poll_id, )
+                    )
+                    self.write({'status': 'SUCCESS'})
+                except Exception as e:
+                    if DEBUG:
+                        print(e)
+                    self.write({'status': 'ERROR'})
         await db.close()
 
 
@@ -77,29 +97,39 @@ class PollAddHandler(RequestHandler):
     async def post(self):
         self.set_header('Content-Type', 'application/json')
         db = await self.get_db()
-        try:
-            poll_id = int(self.get_argument('id'))
-            order = self.get_argument('order')
-            year = self.get_argument('year')
-            subject = self.get_argument('subject')
-            body = self.get_argument('body')
-            if poll_id != -1:
-                await db.execute(
-                    'UPDATE "poll" SET "order"=%s, "year"=%s, "subject"=%s, "body"=%s'
-                    ' WHERE "id"=%s AND "status"=1',
-                    (order, year, subject, body, poll_id)
-                )
+        uid = self.get_secure_cookie('uid')
+
+        if uid == None:
+            self.write({'status': 'NOT LOGINED'})
+        else:
+            uid = int(uid)
+            user = await get_user(db, uid)
+            if user.power != 1:
+                self.write({'status': 'PERMISSION DENIED'})
             else:
-                await db.execute(
-                    'INSERT INTO "poll" ("order", "year", "subject", "body", "status")'
-                    ' VALUES (%s, %s, %s, %s, 1)',
-                    (order, year, subject, body)
-                )
-            self.write({'status': 'SUCCESS'})
-        except Exception as e:
-            if DEBUG:
-                print(e)
-            self.write({'status': 'ERROR'})
+                try:
+                    poll_id = int(self.get_argument('id'))
+                    order = self.get_argument('order')
+                    year = self.get_argument('year')
+                    subject = self.get_argument('subject')
+                    body = self.get_argument('body')
+                    if poll_id != -1:
+                        await db.execute(
+                            'UPDATE "poll" SET "order"=%s, "year"=%s, "subject"=%s, "body"=%s'
+                            ' WHERE "id"=%s AND "status"=1',
+                            (order, year, subject, body, poll_id)
+                        )
+                    else:
+                        await db.execute(
+                            'INSERT INTO "poll" ("order", "year", "subject", "body", "status")'
+                            ' VALUES (%s, %s, %s, %s, 1)',
+                            (order, year, subject, body)
+                        )
+                    self.write({'status': 'SUCCESS'})
+                except Exception as e:
+                    if DEBUG:
+                        print(e)
+                    self.write({'status': 'ERROR'})
         await db.close()
 
 
@@ -107,16 +137,26 @@ class QaHandler(RequestHandler):
     async def post(self):
         self.set_header('Content-Type', 'application/json')
         db = await self.get_db()
-        data = []
-        async for row in db.execute(
-            'SELECT * FROM "qa" WHERE "status"=1'
-            ' ORDER BY "order"'
-        ):
-            element = {}
-            for key in row:
-                element[key] = row[key]
-            data.append(element)
-        self.write({'data': data})
+        uid = self.get_secure_cookie('uid')
+
+        if uid == None:
+            self.write({'status': 'NOT LOGINED'})
+        else:
+            uid = int(uid)
+            user = await get_user(db, uid)
+            if user.power != 1:
+                self.write({'status': 'PERMISSION DENIED'})
+            else:
+                data = []
+                async for row in db.execute(
+                    'SELECT * FROM "qa" WHERE "status"=1'
+                    ' ORDER BY "order"'
+                ):
+                    element = {}
+                    for key in row:
+                        element[key] = row[key]
+                    data.append(element)
+                self.write({'status': 'SUCCESS', 'data': data})
         await db.close()
 
 
@@ -124,17 +164,27 @@ class QaDeleteHandler(RequestHandler):
     async def post(self):
         self.set_header('Content-Type', 'application/json')
         db = await self.get_db()
-        try:
-            poll_id = self.get_argument('id')
-            await db.execute(
-                'UPDATE "qa" SET "status"=0 WHERE "id"=%s',
-                (poll_id, )
-            )
-            self.write({'status': 'SUCCESS'})
-        except Exception as e:
-            if DEBUG:
-                print(e)
-            self.write({'status': 'ERROR'})
+        uid = self.get_secure_cookie('uid')
+
+        if uid == None:
+            self.write({'status': 'NOT LOGINED'})
+        else:
+            uid = int(uid)
+            user = await get_user(db, uid)
+            if user.power != 1:
+                self.write({'status': 'PERMISSION DENIED'})
+            else:
+                try:
+                    poll_id = self.get_argument('id')
+                    await db.execute(
+                        'UPDATE "qa" SET "status"=0 WHERE "id"=%s',
+                        (poll_id, )
+                    )
+                    self.write({'status': 'SUCCESS'})
+                except Exception as e:
+                    if DEBUG:
+                        print(e)
+                    self.write({'status': 'ERROR'})
         await db.close()
 
 
@@ -142,28 +192,39 @@ class QaAddHandler(RequestHandler):
     async def post(self):
         self.set_header('Content-Type', 'application/json')
         db = await self.get_db()
-        try:
-            qa_id = int(self.get_argument('id'))
-            order = self.get_argument('order')
-            question = self.get_argument('question')
-            answer = self.get_argument('answer')
-            if qa_id != -1:
-                await db.execute(
-                    'UPDATE "qa" SET "order"=%s, "question"=%s, "answer"=%s'
-                    ' WHERE "id"=%s AND "status"=1',
-                    (order, question, answer, qa_id)
-                )
+        uid = self.get_secure_cookie('uid')
+
+        if uid == None:
+            self.write({'status': 'NOT LOGINED'})
+        else:
+            uid = int(uid)
+            user = await get_user(db, uid)
+            if user.power != 1:
+                self.write({'status': 'PERMISSION DENIED'})
             else:
-                await db.execute(
-                    'INSERT INTO "qa" ("order", "question", "answer", "status")'
-                    ' VALUES (%s, %s, %s, 1)',
-                    (order, question, answer)
-                )
-            self.write({'status': 'SUCCESS'})
-        except Exception as e:
-            if DEBUG:
-                print(e)
-            self.write({'status': 'ERROR'})
+                try:
+                    qa_id = int(self.get_argument('id'))
+                    order = self.get_argument('order')
+                    question = self.get_argument('question')
+                    answer = self.get_argument('answer')
+                    if qa_id != -1:
+                        await db.execute(
+                            'UPDATE "qa" SET "order"=%s, "question"=%s, "answer"=%s'
+                            ' WHERE "id"=%s AND "status"=1',
+                            (order, question, answer, qa_id)
+                        )
+                    else:
+                        await db.execute(
+                            'INSERT INTO "qa" ("order", "question", "answer", "status")'
+                            ' VALUES (%s, %s, %s, 1)',
+                            (order, question, answer)
+                        )
+                    self.write({'status': 'SUCCESS'})
+                except Exception as e:
+                    if DEBUG:
+                        print(e)
+                    self.write({'status': 'ERROR'})
+        await db.close()
 
         
 class ManageHandler(RequestHandler):
@@ -171,15 +232,16 @@ class ManageHandler(RequestHandler):
         self.set_header('Content-Type', 'application/json')
         db = await self.get_db()
         uid = self.get_secure_cookie('uid')
-        if uid:
+
+        if uid == None:
+            self.write({'status': 'NOT LOGINED'})
+        else:
             uid = int(uid)
             user = await get_user(db, uid)
-            if user.power == 1:
-                self.write({'status': 'SUCCESS'})
-            else:
+            if user.power != 1:
                 self.write({'status': 'PERMISSION DENIED'})
-        else:
-            self.write({'status': 'NOT LOGINED'})
+            else:
+                self.write({'status': 'SUCCESS'})
         await db.close()
 
 
