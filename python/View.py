39d746cf0f03d810133d5070edacb14958f130f7
JamesQAQ <luniacslime@gmail.com>
2016-11-13 17:00:57 +0000

    Fix bug: bug of missing application question
    Now the view will check each answer if exist.
    
    TEST=Runable

--- python/View.py
+++ python/View.py
@@ -761,18 +761,32 @@ class ApplicationHandler(RequestHandler):
                 class_type = int(self.get_argument('class_type'))
                 data = []
                 if user.signup_status & (1 << (class_type - 1)):
+                    id_records = {}
                     async for row in db.execute(
                         'SELECT q."id", q."order", q."description", a."description" as "answer" FROM "application_question" q'
                         ' JOIN "application_answer" a'
                         ' ON q."id"=a."qid"'
-                        ' WHERE q."class_type"=%s AND a."uid"=%s AND q."status"=1'
-                        ' ORDER BY "order"',
+                        ' WHERE q."class_type"=%s AND a."uid"=%s AND q."status"=1',
                         (class_type, uid)
                     ):
                         element = {}
                         for key in row:
                             element[key] = row[key]
                         data.append(element)
+                        id_records[row.id] = True
+
+                    async for row in db.execute(
+                        'SELECT * FROM "application_question"'
+                        ' WHERE "class_type"=%s AND "status"=1'
+                        ' ORDER BY "order"',
+                        (class_type, )
+                    ):
+                        if row.id not in id_records:
+                            element = {}
+                            for key in row:
+                                element[key] = row[key]
+                            data.append(element)
+
                 else:
                     async for row in db.execute(
                         'SELECT * FROM "application_question"'
@@ -884,29 +898,36 @@ class ApplicationAnswerHandler(RequestHandler):
                     self.write({'status': 'PERMISSION DENIED'})
                 else:                
                     data = json.loads(self.get_argument('data'))
-                    if user.signup_status & (1 << (class_type - 1)):
-                        for obj in data:
-                            await db.execute(
-                                'UPDATE "application_answer"'
-                                ' SET "description"=%s'
-                                ' WHERE "uid"=%s AND "qid"=%s',
-                                (obj['answer'], uid, obj['id'])
-                            )
-                    else:
-                        for obj in data:
-                            legal = False
+                    for obj in data:
+                        legal = False
+                        async for row in db.execute(
+                            'SELECT * FROM "application_question"'
+                            ' WHERE "class_type"=%s AND "id"=%s',
+                            (class_type, obj['id'])
+                        ):
+                            legal = True
+                        if legal:
+                            exist = False
                             async for row in db.execute(
-                                'SELECT * FROM "application_question"'
-                                ' WHERE "class_type"=%s AND "id"=%s',
-                                (class_type, obj['id'])
+                                'SELECT * FROM "application_answer"'
+                                ' WHERE "uid"=%s AND "qid"=%s',
+                                (uid, obj['id'])
                             ):
-                                legal = True
-                            if legal:
+                                exist = True
+                            if exist:
+                                await db.execute(
+                                    'UPDATE "application_answer"'
+                                    ' SET "description"=%s'
+                                    ' WHERE "uid"=%s AND "qid"=%s',
+                                    (obj['answer'], uid, obj['id'])
+                                )
+                            else:
                                 await db.execute(
                                     'INSERT INTO "application_answer" ("uid", "qid", "description")'
                                     ' VALUES (%s, %s, %s)',
                                     (uid, obj['id'], obj['answer'])
                                 )
+                    if (user.signup_status & (1 << (class_type - 1))) == 0:
                         await db.execute(
                             'UPDATE "user" SET "signup_status"=%s WHERE "id"=%s',
                             (user.signup_status | (1 << (class_type - 1)), uid)
