794cb5c5d01795218bfc326da45251301f15f268
Liang-Chieh Chen <luniacslime@gmail.com>
2017-02-09 03:36:49 +0800

    Refactor: register.js
    As title.
    
    TEST=Runable

--- http/js/register.js
+++ http/js/register.js
@@ -1,69 +1,74 @@
 'use strict'
 
-var register = new function() {
-    var that = this;
+var register = register || {};
 
-    that.load = function() {
-        var t_options = $('#options-templ').html();
-        var t_school_options = $('#school-options-templ').html();
-        var j_gender = $('#gender');
-        var j_school_type = $('#school_type');
-        var j_grade = $('#grade');
-
-        function set_grade_option(e) {
-            var max_grade = parseInt(j_school_type.find(':selected').attr('max_grade'));
-            var data = [];
-            for (var i = 1; i <= max_grade; i++)
-                data.push({'id': i, 'value': i});
-            j_grade.html(Mustache.render(t_options, {'data': data}));
-        }
-
-        j_school_type.on('change', set_grade_option);
-
-        $.post('/spt/d/register/get_options', {}, function(res) {
-            if (res.status == 'SUCCESS') {
-                j_gender.html(Mustache.render(t_options, {'data': res.data.gender}));
-                j_school_type.html(Mustache.render(t_school_options, {'data': res.data.school_type}));
-                set_grade_option(undefined);
-            }
-            ajax_done();
-        });
-
-        $('#submit').on('click', function(e) {
-            if (confirm('確認送出？')) {
-                var params = window.location.search.replace('?', '').split('&');
-                var data = {};
-                params.forEach(function(param) {
-                    var arr = param.split('=');
-                    data[arr[0]] = arr[1];
-                });
-
-                var done = true;
-                var fields = ['full_name', 'gender', 'school', 'school_type', 'grade', 'address', 'phone'];
-                fields.forEach(function(name) {
-                    data[name] = $('#' + name).val();
-                    if (data[name] == '')
-                        done = false;
-                });
-
-                if (done) {
-                    ajax_start();
-                    $.post('/spt/d/register/second', data, function(res) {
-                        if (res.status == 'SUCCESS') {
-                            show_message('基本資料填寫完成，請回個人頁面登入後進行報名。');
-                            reload_page('/spt/indiv/');
-                        }
-                        else if (res.status == 'FAILED')
-                            show_message('參數錯誤！');
-                        else if (res.status == 'ERROR')
-                            show_message('系統錯誤！');
-                        ajax_done();
-                    });
-                } else {
-                    show_message('尚有欄位未填寫完成');
-                }
-            }
-        });
-    };
+register.load = function() {
+    $.post('/spt/d/register/get_options', {}, register.render_data);
 };
 
+register.render_data = function(res) {
+    if (res.status == 'SUCCESS') {
+        $('#gender').html(Mustache.render(t_options, {'data': res.data.gender}));
+        $('#school_type').html(Mustache.render(t_school_options, {'data': res.data.school_type}));
+        $('#school_type').on('change', set_grade_option);
+        register.set_grade_option(undefined);
+        register.init_submit_button();
+    }
+    ajax_done();
+};
+
+register.set_grade_option = function(event) {
+    var max_grade = parseInt(j_school_type.find(':selected').attr('max_grade'));
+    var data = [];
+    for (var i = 1; i <= max_grade; i++)
+        data.push({'id': i, 'value': i});
+    $('#grade').html(Mustache.render(t_options, {'data': data}));
+};
+
+register.init_submit_button = function() {
+    $('#submit').on('click', function(event) {
+        if (confirm('確認送出？')) {
+            var data = {}
+            if (register.set_submit_data(data)) {
+                ajax_start(data);
+            } else {
+                show_message('尚有欄位未填寫完成');
+            }
+        }
+    });
+};
+
+register.set_submit_data = function(data) {
+    var params = window.location.search.replace('?', '').split('&');
+    params.forEach(function(param) {
+        var arr = param.split('=');
+        data[arr[0]] = arr[1];
+    });
+
+    var fields = ['full_name', 'gender', 'school', 'school_type', 'grade', 'address', 'phone'];
+    fields.forEach(function(name) {
+        data[name] = $('#' + name).val();
+        if (data[name] == '') {
+            return false;
+        }
+    });
+
+    return true;
+};
+
+register.submit_data = function(data) {
+    ajax_start();
+    $.post('/spt/d/register/second', data, register.handle_result);
+};
+
+register.handle_result = function(res) {
+    if (res.status == 'SUCCESS') {
+        show_message('基本資料填寫完成，請回個人頁面登入後進行報名。');
+        reload_page('/spt/indiv/');
+    }
+    else if (res.status == 'FAILED')
+        show_message('參數錯誤！');
+    else if (res.status == 'ERROR')
+        show_message('系統錯誤！');
+    ajax_done();
+};
