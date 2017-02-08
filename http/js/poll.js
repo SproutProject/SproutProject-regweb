7613cdec185af4d0b782499899fd58e78cac6aa4
Liang-Chieh Chen <luniacslime@gmail.com>
2017-02-09 00:52:56 +0800

    Refactor: poll.js
    Changed the organization.
    
    TEST=None

--- http/js/poll.js
+++ http/js/poll.js
@@ -1,28 +1,34 @@
 'use strict'
 
-var poll = new function() {
-    var that = this;
+var poll = poll || {};
 
-    that.load = function() {
-        var template = $('#template').html();
+poll.load = function() {
+    $.post('/spt/d/poll/get_all', {}, poll.render_data);
+}
 
-        $.post('/spt/d/poll/get_all', {}, function(res) {
-            $('#list').html(Mustache.render(template, res));
+poll.render_data = function(res) {
+    var template = $('#template').html();
+    $('#list').html(Mustache.render(template, res));
 
-            $('#list div.item').on('click', function(e) {
-                var j_this = $(this);
-                $('#list').find('div.subject').hide();
-                j_this.addClass('active');
-            });
+    poll.init_poll();
+    poll.init_close_button();
 
-            $('#list button.close').on('click', function(e) {
-                var j_item = $(this).parents('div.item');
-                $('#list').find('div.subject').show();
-                j_item.removeClass('active');
-                return false;
-            });
+    ajax_done();
+}
 
-            ajax_done();
-        });
-    };
+poll.init_poll = function() {
+    $('#list div.item').on('click', function(e) {
+        var poll_item = $(this);
+        $('#list').find('div.subject').hide();
+        poll_item.addClass('active');
+    });
+}
+
+poll.init_close_button = function() {
+    $('#list button.close').on('click', function(e) {
+        var poll_item = $(this).parents('div.item');
+        $('#list').find('div.subject').show();
+        poll_item.removeClass('active');
+        return false;
+    });
 }
