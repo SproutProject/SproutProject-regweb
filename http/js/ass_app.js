8083416cca850339728bbcf5d959c84b12ddc1cd
Liang-Chieh Chen <luniacslime@gmail.com>
2017-02-09 22:45:52 +0800

    Refactor: ass_app.js
    As title.
    
    TEST=Runable

--- http/js/ass_app.js
+++ http/js/ass_app.js
@@ -1,69 +1,98 @@
 'use strict'
 
-var ass_app = new function() {
-    var that = this;
+var ass_app = ass_app || {};
 
-    that.load = function() {
-        var t_app = $('#app-templ').html();
+ass_app.load = function() {
+    $.post('/spt/d/application/get_all',
+        {'class_type': class_type},
+        ass_app.render_data
+    )
+};
 
-        $.post('/spt/d/application/get_all', {
-            'class_type': class_type
-        }, function(res) {
-            res.title_value = function() {
-                var arr = ['C 語法班', 'Python 語法班', '算法班'];
-                return arr[class_type - 1];
-            }
+ass_app.render_data = function(res) {
+    var template = $('#app-templ').html();
+    res.title_value = function() {
+        var arr = ['C 語法班', 'Python 語法班', '算法班'];
+        return arr[class_type - 1];
+    }
+    res.description_value = function() {
+        return this.description.slice(0, 10) + '...';
+    }
 
-            res.description_value = function() {
-                return this.description.slice(0, 10) + '...';
-            }
+    $('#ass_app').html(Mustache.render(template, res));
+    if (ass_mode_on) resizeSpecial();
 
-            $('#ass_app').html(Mustache.render(t_app, res));
-            if (ass_mode_on) resizeSpecial();
+    ass_app.init_modify_button();
+    ass_app.init_open_button();
+    ass_app.init_close_button();
+    ass_app.init_delete_button();
 
-            $('button.modify').on('click', function(e) {
-                if (confirm('確認修改？')) {
-                    var j_edit = $(this).parent();
-                    var app_id = j_edit.attr('appid');
-                    if (app_id == undefined)
-                        app_id = -1;
-                    var order = parseInt(j_edit.find('input.order').val());
-                    var description = j_edit.find('textarea').val();
-                    ajax_start();
+    ajax_done();
+};
 
-                    $.post('/spt/d/application/update_question', {
-                        'id': app_id,
-                        'order': order,
-                        'class_type': class_type,
-                        'description': description
-                    }, function(res) {
-                        reload_page('/spt/ass_app/?type=' + class_type);
-                    });
-                }
-            });
+ass_app.init_modify_button = function() {
+     $('button.modify').on('click', function(e) {
+        if (confirm('確認修改？')) {
+            ass_app.update_question($(this).parent());
+        }
+    });
+};
 
-            $('button.open').on('click', function(e) {
-                var j_edit = $(this).parent().next();
-                $('div.edit.active').removeClass('active');
-                j_edit.addClass('active');
-            });
+ass_app.update_question = function(j_edit) {
+    var app_id = j_edit.attr('appid');
+    if (app_id == undefined)
+        app_id = -1;
+    var order = parseInt(j_edit.find('input.order').val());
+    var description = j_edit.find('textarea').val();
 
-            $('button.close').on('click', function(e) {
-                $('div.edit.active').removeClass('active');
-            });
+    var data = {
+        'id': app_id,
+        'order': order,
+        'class_type': class_type,
+        'description': description
+    };
 
-            $('button.delete').on('click', function(e) {
-                if (confirm('確認刪除？')) {
-                    ajax_start();
-                    $.post('/spt/d/application/del_question', {
-                        'id': $(this).parent().attr('appid'),
-                    }, function(res) {
-                        reload_page('/spt/ass_app/?type=' + class_type);
-                    });
-                }
-            });
+    ajax_start();
+    $.post('/spt/d/application/update_question', data, ass_app.handle_result);
+};
 
-            ajax_done();
-        });
-    }; 
-}
+ass_app.init_open_button = function() {
+    $('button.open').on('click', function(e) {
+        var j_edit = $(this).parent().next();
+        $('div.edit.active').removeClass('active');
+        j_edit.addClass('active');
+    });
+};
+
+ass_app.init_close_button = function() {
+    $('button.close').on('click', function(e) {
+        $('div.edit.active').removeClass('active');
+    });
+};
+
+ass_app.init_delete_button = function() {
+    $('button.delete').on('click', function(e) {
+        if (confirm('確認刪除？')) {
+            var data = {
+                'id': $(this).parent().attr('appid'),
+            };
+
+            ajax_start();
+            $.post('/spt/d/application/del_question', data, ass_app.handle_result);
+        }
+    });
+};
+
+ass_app.handle_result = function(res) {
+    if (res.status == 'SUCCESS') {
+        reload_page('/spt/ass_app/?type=' + class_type);
+    } else if (res.status == 'NOT LOGINED') {
+        show_message('尚未登入');
+    } else if (res.status == 'PERMISSION DENIED') {
+        show_message('權限不足。');
+    } else if (res.status == 'ERROR') {
+        show_message('系統錯誤！');
+    }
+
+    ajax_done();
+};
