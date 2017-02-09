aa0d97e08ff8a70cff0279b23e483813125f6cfa
Liang-Chieh Chen <luniacslime@gmail.com>
2017-02-09 23:47:12 +0800

    Refactor: ass_god.js
    As title.
    
    TEST=Runable

--- http/js/ass_god.js
+++ http/js/ass_god.js
@@ -1,31 +1,39 @@
 'use strict'
 
-var ass_god = new function() {
-    var that = this;
-    
-    that.load = function() {
-        if (ass_mode_on) resizeSpecial();
-        
-        $('#submit').on('click', function(e) {
-            var mail = $('#mail').val();
-            var power = $('#power').val();
+var ass_god = ass_god || {};
 
-            ajax_start();
-            $.post('/spt/d/user/set_power', {
-                'mail': mail,
-                'power': power,
-            }, function(res) {
-                if (res.status == 'SUCCESS')
-                    show_message('Done.');
-                else if (res.status == 'FAILED')
-                    show_message('You are not ASS god!');
-                else if (res.status == 'ERROR')
-                    show_message('系統錯誤！');
-                ajax_done();
-            });
-        });
+ass_god.load = function() {
+    ass_god.render_data();
+};
 
-        ajax_done();
-    }
-}
+ass_god.render_data = function() {
+    if (ass_mode_on) resizeSpecial();
+    ass_god.init_submit_button();
+    ajax_done();
+};
 
+ass_god.init_submit_button = function() {
+    $('#submit').on('click', function(e) {
+        var mail = $('#mail').val();
+        var power = $('#power').val();
+        var data = {
+            'mail': mail,
+            'power': power,
+        };
+
+        ajax_start();
+        $.post('/spt/d/user/set_power', data, ass_god.handle_result);
+    });
+};
+
+ass_god.handle_result = function(res) {
+    if (res.status == 'SUCCESS')
+        show_message('Done.');
+    else if (res.status == 'NOT LOGIN')
+        show_message('Not login.');
+    else if (res.status == 'PERMISSION DENIED')
+        show_message('You are not ASS god!');
+    else if (res.status == 'ERROR')
+        show_message('系統錯誤！');
+    ajax_done();
+};
