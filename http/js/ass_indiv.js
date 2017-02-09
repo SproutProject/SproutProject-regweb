f8c33af6f94c562d98a62fa13463ba65b981b9a5
Liang-Chieh Chen <luniacslime@gmail.com>
2017-02-10 03:39:12 +0800

    Refactor: ass_indiv.js
    As title.
    
    TEST=Runable

--- http/js/ass_indiv.js
+++ http/js/ass_indiv.js
@@ -1,82 +1,87 @@
 'use strict'
 
-var ass_indiv = new function() {
-    var that = this;
-    
-    that.load = function() {
-        var t_indiv_data = $('#indiv-data-templ').html();
-        var t_user_data = $('#user-data-templ').html();
-        var j_indiv = $('#ass_indiv');
+var ass_indiv = ass_indiv || {};
 
-        j_indiv.html(Mustache.render(t_indiv_data));
-        if (ass_mode_on) resizeSpecial();
+ass_indiv.load = function() {
+    var template = $('#indiv-data-templ').html();
+    $('#ass_indiv').html(Mustache.render(template));
+    if (ass_mode_on) resizeSpecial();
 
-        $('#rule_test').on('click', function(e) {
-            reload_page('/spt/ass_rule_test/');
-        });
+    ass_indiv.init_buttons();
+    ass_indiv.init_show_button();
 
-        $('#c_class').on('click', function(e) {
-            reload_page('/spt/ass_app/?type=1');
-        });
-        $('#python_class').on('click', function(e) {
-            reload_page('/spt/ass_app/?type=2');
-        });
-        $('#algorithm_class').on('click', function(e) {
-            reload_page('/spt/ass_app/?type=3');
-        });
+    ajax_done();
+};
 
-        $('#show_indiv_data').on('click', function(e) {
-            ajax_start();
-            $.post('/spt/d/user/get_all_user_data', {}, function(res) {
-                res.power_value = function() {
-                    if (this.power == -1)
-                        return '未完成註冊'
-                    var arr = ['一般', '管理者', '神'];
-                    return arr[this.power];
-                };
+ass_indiv.init_buttons = function() {
+    $('#rule_test').on('click', function(e) {
+        reload_page('/spt/ass_rule_test/');
+    });
 
-                res.signup_status_value = function() {
-                    var res = [];
-                    if (this.signup_status & 1)
-                        res.push('C');
-                    if (this.signup_status & 2)
-                        res.push('Py');
-                    if (this.signup_status & 4)
-                        res.push('Algo');
-                    return res.join(', ');
-                };
+    $('#c_class').on('click', function(e) {
+        reload_page('/spt/ass_app/?type=1');
+    });
+    $('#python_class').on('click', function(e) {
+        reload_page('/spt/ass_app/?type=2');
+    });
+    $('#algorithm_class').on('click', function(e) {
+        reload_page('/spt/ass_app/?type=3');
+    });
+};
 
-                res.rule_test_value = function() {
-                    return (this.rule_test) ? '通過' : '未通過';
-                };
+ass_indiv.init_show_button = function() {
+    $('#show_indiv_data').on('click', function(e) {
+        ajax_start();
+        $.post('/spt/d/user/get_all_user_data', {}, ass_indiv.handle_user_data);
+    });
+};
 
-                res.pre_test_value = function() {
-                    return (this.pre_test) ? '通過' : '未通過';
-                };
+ass_indiv.handle_user_data = function(res) {
+    var template = $('#user-data-templ').html();
+    res.power_value = function() {
+        if (this.power == -1)
+            return '未完成註冊'
+        var arr = ['一般', '管理者', '神'];
+        return arr[this.power];
+    };
+    res.signup_status_value = function() {
+        var res = [];
+        if (this.signup_status & 1)
+            res.push('C');
+        if (this.signup_status & 2)
+            res.push('Py');
+        if (this.signup_status & 4)
+            res.push('Algo');
+        return res.join(', ');
+    };
+    res.rule_test_value = function() {
+        return (this.rule_test) ? '通過' : '未通過';
+    };
+    res.pre_test_value = function() {
+        return (this.pre_test) ? '通過' : '未通過';
+    };
 
-                $("#sign_up").hide();
-                j_indiv.append(Mustache.render(t_user_data, res));
+    $("#sign_up").hide();
+    $('#ass_indiv').append(Mustache.render(template, res));
 
-                $('button.open').on('click', function(e) {
-                    var j_edit = $(this).parent().next();
-                    $("div.edit.active").removeClass('active');
-                    j_edit.addClass('active');
-                });
+    ass_indiv.init_user_data_buttons();
 
-                $('button.close').on('click', function(e) {
-                    $("div.edit.active").removeClass('active');
-                });
+    ajax_done();
+};
 
-                $('button#return_sign_up').on('click', function(e) {
-                    $("#user_data").remove();
-                    $("#sign_up").show();
-                });
+ass_indiv.init_user_data_buttons = function() {
+    $('button.open').on('click', function(e) {
+        var j_edit = $(this).parent().next();
+        $("div.edit.active").removeClass('active');
+        j_edit.addClass('active');
+    });
 
-                ajax_done();
-            });
-        });
+    $('button.close').on('click', function(e) {
+        $("div.edit.active").removeClass('active');
+    });
 
-        ajax_done();
-    }
-}
-
+    $('button#return_sign_up').on('click', function(e) {
+        $("#user_data").remove();
+        $("#sign_up").show();
+    });
+};
