93ee9ed275b2794ce32aa6ce1e1f7983b5d07523
Liang-Chieh Chen <luniacslime@gmail.com>
2017-02-09 23:09:56 +0800

    Refactor: ass_denied.js
    As title.
    
    TEST=Runable

--- http/js/ass_denied.js
+++ http/js/ass_denied.js
@@ -1,22 +1,24 @@
 'use strict'
 
-var ass_denied = new function() {
-    var that = this;
+var ass_denied = ass_denied || {};
 
-    that.load = function() {
-        var t_permission_denied = $('#permission-denied-templ').html();
-        var t_original_nav = $('#original-nav-templ').html();
+ass_denied.load = function() {
+    ass_denied.render_data();
+};
 
-        $('#ass_denied').html(Mustache.render(t_permission_denied));
-        $('#nav').html(Mustache.render(t_original_nav));
+ass_denied.render_data = function() {
+    var t_permission_denied = $('#permission-denied-templ').html();
+    var t_original_nav = $('#original-nav-templ').html();
 
-        resizeYoutubeEmbed();
-        $(window).resize(resizeYoutubeEmbed);
+    $('#ass_denied').html(Mustache.render(t_permission_denied));
+    $('#nav').html(Mustache.render(t_original_nav));
 
-        ajax_done();
-    };
-}
+    ass_denied.resize_youtube_embed();
+    $(window).resize(ass_denied.resize_youtube_embed);
 
-function resizeYoutubeEmbed() {
+    ajax_done();
+};
+
+ass_denied.resize_youtube_embed = function() {
     $('iframe').height($('iframe').width() * (9 / 16));
-}
+};
