9ad7ae9db1eb7457614d9db59cac8319ca41ccab
Liang-Chieh Chen <luniacslime@gmail.com>
2017-02-10 02:09:56 +0800

    Refactor: rule_test.js
    As title.
    
    TEST=Runable

--- http/js/rule_test.js
+++ http/js/rule_test.js
@@ -1,46 +1,47 @@
 'use strict'
 
-var rule_test = new function() {
-    var that = this;
+var rule_test = rule_test || {};
 
-    that.load = function() {
-        var t_question = $('#question-templ').html();
+rule_test.load = function() {
+    $.post('/spt/d/rule_test/get_question', {}, rule_test.render_data);
+};
 
-        $.post('/spt/d/rule_test/get_question', {}, function(res) {
-            $('#rule_test').html(Mustache.render(t_question, res));
+rule_test.render_data = function(res) {
+    var template = $('#question-templ').html();
+    $('#rule_test').html(Mustache.render(template, res));
+    rule_test.init_submit_button();
+    ajax_done();
+};
 
-            $('button.submit').on('click', function(e) {
-                var data = {};
-                var question_list = $('div.question');
-                for (var i = 0; i < question_list.size(); i++) {
-                    var qid = $(question_list[i]).attr('qid');
-                    var aid = $(question_list[i]).find('input:checked').val();
-                    if (aid == undefined) {
-                        show_message('尚未完成作答。');
-                        return;
-                    }
-                    data[qid] = aid;
-                }
+rule_test.init_submit_button = function() {
+    $('button.submit').on('click', function(e) {
+        var data = {};
+        var question_list = $('div.question');
+        for (var i = 0; i < question_list.size(); i++) {
+            var qid = $(question_list[i]).attr('qid');
+            var aid = $(question_list[i]).find('input:checked').val();
+            if (aid == undefined) {
+                show_message('尚未完成作答。');
+                return;
+            }
+            data[qid] = aid;
+        }
 
-                ajax_start();
-                $.post('/spt/d/rule_test/answer', {
-                    'data': JSON.stringify(data)
-                }, function(res) {
-                    if (res.status == 'SUCCESS') {
-                        show_message('恭喜完成作答！');
-                        reload_page('/spt/indiv/');
-                    }
-                    else if (res.status == 'WRONG')
-                        show_message('答案錯誤！');
-                    else if (res.status == 'ERROR')
-                        show_message('系統錯誤！');
-                    else if (res.status == 'PERMISSION DENIED')
-                        show_message('不合法的行為。');                        
-                    ajax_done();
-                });
-            });
+        ajax_start();
+        $.post('/spt/d/rule_test/answer', {'data': JSON.stringify(data)}, rule_test.handle_result);
+    });
+};
 
-            ajax_done();
-        });
-    };
-}
+rule_test.handle_result = function(res) {
+    if (res.status == 'SUCCESS') {
+        show_message('恭喜完成作答！');
+        reload_page('/spt/indiv/');
+    } else if (res.status == 'WRONG') {
+        show_message('答案錯誤！');
+    } else if (res.status == 'ERROR') {
+        show_message('系統錯誤！');
+    } else if (res.status == 'PERMISSION DENIED') {
+        show_message('不合法的行為。');
+    }
+    ajax_done();
+};
